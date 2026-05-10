82827c530e1bc703e3bcbdc134779bd15e65c4cc
NesaPrasath <106434404+NesaPrasath@users.noreply.github.com>
2026-05-10 09:10:48 +0530

    change-10052026-02

--- Service.js
+++ Service.js
@@ -10,9 +10,9 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", require("./routes/contactRoutes"));
-app.use("/api", require("./routes/sessionRoutes"));
-app.use("/api", require("./routes/authRoutes"));
+app.use("/api", require("./routes/contactRoutes.js"));
+app.use("/api", require("./routes/sessionRoutes.js"));
+app.use("/api", require("./routes/authRoutes.js"));
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
